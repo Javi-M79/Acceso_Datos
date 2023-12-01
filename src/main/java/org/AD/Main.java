7f5284f3225bc7ee6a440583946fe3e7ab5511bc
Silx79 <javier.mira46@gmail.com>
2023-12-01 20:53:35 +0100

    Comprobacion de lectura del JSON

--- src/main/java/org/AD/Main.java
+++ src/main/java/org/AD/Main.java
@@ -1,18 +1,21 @@
 package org.AD;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         try {
 
             URL url = new URL("https://dummyjson.com/products");
             HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+            BufferedReader lectura = new BufferedReader (new InputStreamReader(conexion.getInputStream()));
+//            System.out.println(lectura.readLine());//Comprobar que lee correctamente el JSON
         } catch (IOException e) {
             System.out.println("Error enb la conexion I/O");
         }
